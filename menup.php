f9bc89d6b9ff2d6beed7a2d3117f19e5c1bcc345
thepattj <jdej261@gmail.com>
2017-11-29 08:18:41 -0600

    Inicios

---
+++ menup.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio de Sesión</title>
+	<meta charset="utf-8">
+	<script type="text/javascript" src=""></script>
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+	<script src="js/jquery/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="js/bootstrap/bootstrap.min.js"></script>
+</head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.html">Cotizador Incapital QRO</a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent"></div>
+</nav>
+<body>
+<h1>BIENVENIDO</h1>
+<h6>Selecciona la actividad</h6>
+</body>
+
+</html>
